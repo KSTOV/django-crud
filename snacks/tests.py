0341d0344fb4977bf8505b3eeeba896a86c3adea
KLan Stovall <kland13@gmail.com>
2022-01-09 22:25:10 -0800

    lab complete

---
+++ snacks/tests.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+class SnackTest(TestCase):
+    def test_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+
+    def test_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        url = reverse('snack_detail')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_detail.html')
+
+    def test_status_code(self):
+        url = reverse('snack_detail')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_create.html')
+
+    def test_status_code(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        url = reverse('snack_delete')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_delete.html')
+
+    def test_status_code(self):
+        url = reverse('snack_delete')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        url = reverse('snack_update')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_update.html')
+
+    def test_status_code(self):
+        url = reverse('snack_update')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
